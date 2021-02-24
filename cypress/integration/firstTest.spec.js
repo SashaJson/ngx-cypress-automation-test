d6879f27de83ca25db09a12734da00986bc97694
SashaJson <sashamiller666@gmail.com>
2021-02-24 22:01:05 +0200

    added check boxes testing in first test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -169,4 +169,16 @@ describe('Our first suit', () => {
 
     });
 
+    it.only('check boxes', () => {
+
+        cy.visit('/');
+        cy.contains('Modal & Overlays').click();
+        cy.contains('Toastr').click();
+
+        // cy.get('[type="checkbox"]').check({force: true});
+        cy.get('[type="checkbox"]').eq(0).click({force: true});
+        cy.get('[type="checkbox"]').eq(1).check({force: true});
+
+    });
+
 }); // describe (Our first suit)
