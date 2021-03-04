0c808b8749ae47b062d8c0a1b65c7b56f1588145
SashaJson <sashamiller666@gmail.com>
2021-03-04 23:44:41 +0200

    updated module assert property

--- cypress/integration/assertProperty.spec.js
+++ cypress/integration/assertProperty.spec.js
@@ -4,14 +4,35 @@ describe('Testing assert property', () => {
 
     it('First example testing assert property', () => {
 
+        function selectDayFromCurrent(day) {
+
+            let date = new Date();
+            date.setDate(date.getDate() + day);
+            let futureDay = date.getDate();
+            let futureMonth = date.toLocaleString('default', {month: 'short'});
+            let dateAssert = futureMonth + ' ' + futureDay + ', ' + date.getFullYear();
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+                if (!dateAttribute.includes(futureMonth)) {
+                    cy.get('[data-name="chevron-right"]').click();
+                    selectDayFromCurrent(day);
+                } else {
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click();
+                }
+            });
+
+            return dateAssert;
+
+        }
+
         cy.visit('/');
         cy.contains('Forms').click();
         cy.contains('Datepicker').click();
 
         cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
             cy.wrap(input).click()
-            cy.get('nb-calendar-day-picker').contains('24').click()
-            cy.wrap(input).invoke('prop', 'value').should('contain', 'Mar 24, 2021')
+            let dateAssert = selectDayFromCurrent(300);
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert);
         });
 
     });
