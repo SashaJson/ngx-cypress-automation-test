976edbb6c5e47b592e70eee22292b48b57c0e208
SashaJson <sashamiller666@gmail.com>
2021-04-02 23:23:06 +0300

    init visual testing

---
+++ cypress/integration/visualTest.spec.js
@@ -0,0 +1,14 @@
+describe('Visual test', () => {
+
+    it('should test snapshots', () => {
+        cy.visit('/');
+        cy.contains('Forms').click();
+        cy.contains('Form Layouts').click();
+
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            cy.wrap(firstForm).toMatchImageSnapshot();
+        });
+
+    });
+
+});
